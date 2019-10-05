2f7a03e3efbc4fa650ec72679b0ffd87c0cd0245
Daniel Sullivan <sullydm@gmail.com>
2019-10-04 21:30:48 -0400

    awt tests adjusted. All pass except mss, r2, r2_a
    Looks like yhat and/or ehat is wrong

--- econtools/metrics/tests/test_awt.py
+++ econtools/metrics/tests/test_awt.py
@@ -4,48 +4,34 @@ import pandas as pd
 
 from econtools.metrics.util.testing import RegCompare
 from econtools.metrics.api import reg
-from econtools.metrics.tests.data.src_ols import (ols_std, ols_robust, ols_hc2,
-                                                  ols_hc3, ols_cluster)
+from econtools.metrics.tests.data.src_awt import (awt_std, awt_robust, awt_hc2,
+                                                  awt_hc3, awt_cluster)
 
 
-class TestOLS_std(RegCompare):
+class TestAwt_std(RegCompare):
 
     @classmethod
     def setup_class(cls):
         """Stata reg output from `sysuse auto; reg price mpg`"""
         cls.init(cls)
         cls.precision['vce'] = 6
+        cls.precision['ssr'] = 6
         test_path = path.split(path.relpath(__file__))[0]
         auto_path = path.join(test_path, 'data', 'auto.dta')
         autodata = pd.read_stata(auto_path)
         y = 'price'
         x = ['mpg', 'length']
         cls.result = reg(autodata, y, x, addcons=True, awt_name='mpg')
-        cls.expected = ols_std
+        cls.expected = awt_std
 
 
-class TestOLS_std_y_list(RegCompare):
-
-    @classmethod
-    def setup_class(cls):
-        """Stata reg output from `sysuse auto; reg price mpg`"""
-        cls.init(cls)
-        cls.precision['vce'] = 6
-        test_path = path.split(path.relpath(__file__))[0]
-        auto_path = path.join(test_path, 'data', 'auto.dta')
-        autodata = pd.read_stata(auto_path)
-        y = ['price']
-        x = ['mpg', 'length']
-        cls.result = reg(autodata, y, x, addcons=True, awt_name='mpg')
-        cls.expected = ols_std
-
-
-class TestOLS_hc1(RegCompare):
+class TestAwt_hc1(RegCompare):
 
     @classmethod
     def setup_class(cls):
         """Stata reg output from `sysuse auto; reg price mpg`"""
         cls.init(cls)
+        cls.precision['ssr'] = 6
         test_path = path.split(path.relpath(__file__))[0]
         auto_path = path.join(test_path, 'data', 'auto.dta')
         autodata = pd.read_stata(auto_path)
@@ -53,15 +39,16 @@ class TestOLS_hc1(RegCompare):
         x = ['mpg', 'length']
         cls.result = reg(autodata, y, x, vce_type='hc1', addcons=True,
                          awt_name='mpg')
-        cls.expected = ols_robust
+        cls.expected = awt_robust
 
 
-class TestOLS_hc2(RegCompare):
+class TestAwt_hc2(RegCompare):
 
     @classmethod
     def setup_class(cls):
         """Stata reg output from `sysuse auto; reg price mpg`"""
         cls.init(cls)
+        cls.precision['ssr'] = 6
         test_path = path.split(path.relpath(__file__))[0]
         auto_path = path.join(test_path, 'data', 'auto.dta')
         autodata = pd.read_stata(auto_path)
@@ -69,15 +56,16 @@ class TestOLS_hc2(RegCompare):
         x = ['mpg', 'length']
         cls.result = reg(autodata, y, x, vce_type='hc2', addcons=True,
                          awt_name='mpg')
-        cls.expected = ols_hc2
+        cls.expected = awt_hc2
 
 
-class TestOLS_hc3(RegCompare):
+class TestAwt_hc3(RegCompare):
 
     @classmethod
     def setup_class(cls):
         """Stata reg output from `sysuse auto; reg price mpg`"""
         cls.init(cls)
+        cls.precision['ssr'] = 6
         test_path = path.split(path.relpath(__file__))[0]
         auto_path = path.join(test_path, 'data', 'auto.dta')
         autodata = pd.read_stata(auto_path)
@@ -85,15 +73,16 @@ class TestOLS_hc3(RegCompare):
         x = ['mpg', 'length']
         cls.result = reg(autodata, y, x, vce_type='hc3', addcons=True,
                          awt_name='mpg')
-        cls.expected = ols_hc3
+        cls.expected = awt_hc3
 
 
-class TestOLS_cluster(RegCompare):
+class TestAwt_cluster(RegCompare):
 
     @classmethod
     def setup_class(cls):
         """Stata reg output from `sysuse auto; reg price mpg`"""
         cls.init(cls)
+        cls.precision['ssr'] = 6
         test_path = path.split(path.relpath(__file__))[0]
         auto_path = path.join(test_path, 'data', 'auto.dta')
         autodata = pd.read_stata(auto_path)
@@ -101,7 +90,7 @@ class TestOLS_cluster(RegCompare):
         x = ['mpg', 'length']
         cls.result = reg(autodata, y, x, cluster='gear_ratio', addcons=True,
                          awt_name='mpg')
-        cls.expected = ols_cluster
+        cls.expected = awt_cluster
 
 
 if __name__ == '__main__':
